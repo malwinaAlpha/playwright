9c7fe250fa3eb6a686bc586d2ef75ae02393dbfc
Malwina Kowalczyk <malwinakowalczyk@Malwinas-MacBook-Pro.local>
2025-06-28 18:13:53 +0200

    adding set up and dependencies

---
+++ tests/products.strings.ts
@@ -0,0 +1,9 @@
+export const Products = {
+    check_out_overview: 'Checkout: Overview', //shouldnt it be in the othere section? it is not a product
+    backpack_product: 'Sauce Labs Backpack',
+    bike_light_product: 'Sauce Labs Bike Light',
+    jacket_product: 'Sauce Labs Fleece Jacket',
+    onesie_product: 'Sauce Labs Onesie',
+};
+
+
